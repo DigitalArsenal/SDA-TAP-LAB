5d65d108d4a2e9101a24318528def43748a418e8
TJKoury <tj@digitalarsenal.io>
2023-11-22 09:24:54 -0500

    updates

--- src/lib/StatusBox/StatusBox.svelte
+++ src/lib/StatusBox/StatusBox.svelte
@@ -3,59 +3,63 @@
   import { scenario } from "@/stores/settings.store";
   import { activeEntity } from "@/stores/entity.store";
   import CloseButton from "@/lib/elements/CloseButton.svelte";
-
+  import { Icon } from "svelte-awesome";
+  import { chevronLeft } from "svelte-awesome/icons";
   const { selectedEntity } = scenario;
+  let minimized = false;
 
   const closeModal = () => {
-    $content = undefined;
-    $selectedEntity = null;
+    if ($selectedEntity) {
+      minimized = true;
+    } else {
+      $content = undefined;
+    }
   };
 </script>
 
 <!-- Fullscreen Modal backdrop with margin -->
-<div
-  class="fixed mt-12 pr-2 pl-2 flex justify-end items-start w-full md:w-[38%] h-[25%] md:h-[34%] max-h-[300px] right-0"
->
-  <div class="flex justify-center items-center z-50 w-full h-full">
-    <!-- Modal content -->
-    <div
-      style="background:rgba(48, 51, 54, .9)"
-      class="bg-opacity-75 text-white rounded shadow-xl w-full h-full overflow-auto flex flex-col border border-gray-600"
-    >
-      <!-- Modal header -->
-      <div class="flex-none text-xs md:text-sm">
-        <!-- svelte-ignore a11y-no-static-element-interactions -->
-        <div
-          class="select-none flex justify-between items-center pl-4 pr-3 pt-2 pb-2 md:pt-2 md:pb-2 border-b border-gray-600"
-        >
-          <p class="text-white font-[300]">
-            {#if $title}
-              {@html $title}
-            {:else if $activeEntity}
-              {$activeEntity.id}
-            {:else}
-              Entity
-            {/if}
-          </p>
-          <div class="text-xl">
-            <CloseButton onclick={closeModal} />
+{#if minimized}
+  <button
+    class="rounded fixed flex items-center justify-center top-12 right-[5px] pr-3 cesium-button"
+    on:click={() => {
+      minimized = false;
+    }}><Icon scale={1} data={chevronLeft} /></button
+  >
+{:else}
+  <div
+    class="fixed mt-12 pr-2 pl-2 flex justify-end items-start w-full md:w-[38%] h-[25%] md:h-[34%] max-h-[300px] right-0"
+  >
+    <div class="flex justify-center items-center z-50 w-full h-full">
+      <!-- Modal content -->
+      <div
+        style="background:rgba(48, 51, 54, .9)"
+        class="bg-opacity-75 text-white rounded shadow-xl w-full h-full overflow-auto flex flex-col border border-gray-600"
+      >
+        <!-- Modal header -->
+        <div class="flex-none text-xs md:text-sm">
+          <!-- svelte-ignore a11y-no-static-element-interactions -->
+          <div
+            class="select-none flex justify-between items-center pl-4 pr-3 pt-2 pb-2 md:pt-2 md:pb-2 border-b border-gray-600"
+          >
+            <p class="text-white font-[300]">
+              {#if $title}
+                {@html $title}
+              {:else if $activeEntity}
+                {$activeEntity.id}
+              {:else}
+                Entity
+              {/if}
+            </p>
+            <div class="text-xl">
+              <CloseButton onclick={closeModal} />
+            </div>
           </div>
         </div>
-      </div>
-      <!-- Modal body -->
-      <div class="flex-grow p-1 overflow-auto">
-        <svelte:component this={$template} />
-      </div>
-      <!-- Modal footer 
-      <div class="flex-none">
-        <div class="flex justify-end p-5 border-t border-gray-600">
-          <button
-            class="px-4 py-2 bg-gray-600 rounded hover:bg-gray-700 focus:outline-none focus:ring focus:border-blue-300"
-            on:click={closeModal}>
-            Close
-          </button>
+        <!-- Modal body -->
+        <div class="flex-grow p-1 overflow-auto">
+          <svelte:component this={$template} />
         </div>
-      </div>-->
+      </div>
     </div>
   </div>
-</div>
+{/if}
