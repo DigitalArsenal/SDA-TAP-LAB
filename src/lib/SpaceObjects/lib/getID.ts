450453090bf405e6c211304900d3d5eed67fee25
TJ Koury <tj@digitalarsenal.io>
2023-11-28 16:01:09 -0500

    deploy

--- src/lib/SpaceObjects/lib/getID.ts
+++ src/lib/SpaceObjects/lib/getID.ts
@@ -1,7 +1,7 @@
 export default (data: any) => {
   let id = data.OBJECT_ID;
 
-  if (!id || id.length < 8) {
+  if (!id || id.length < 9) {
     id = `${id}:${data.NORAD_CAT_ID}`;
   }
   return id;
