edd9e4f8e69818173dc719217889a066b43ff9f4
TJ Koury <tj@digitalarsenal.io>
2024-07-31 12:48:17 -0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -54,6 +54,7 @@ const banner = `/*!
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [svelte({
     preprocess: sveltePreprocess(),
     onwarn: (warning, handler) => {
