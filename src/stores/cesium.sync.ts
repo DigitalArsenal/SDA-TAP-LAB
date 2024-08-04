7ad5518c4302eedbca7e21c5354c9bb539b9b6ce
TJKoury <tj@digitalarsenal.io>
2024-08-04 11:44:19 -0400

    deploy

--- src/stores/cesium.sync.ts
+++ src/stores/cesium.sync.ts
@@ -5,9 +5,9 @@ import { scenario } from "@/stores/settings.store";
 import { debounce } from "@/utilities/debounce";
 
 const dataSourceEvents = {
-    dataSourceAdded: writable<DataSource | null>(null),
-    dataSourceMoved: writable<DataSource | null>(null),
-    dataSourceRemoved: writable<DataSource | null>(null)
+  dataSourceAdded: writable<DataSource | null>(null),
+  dataSourceMoved: writable<DataSource | null>(null),
+  dataSourceRemoved: writable<DataSource | null>(null),
 };
 
 let registeredEvents: any = {};
@@ -15,11 +15,12 @@ let registeredStoreSubscriptions: any = {};
 let isUpdatingFromStore = false;
 
 const initEvents = (viewer: Viewer) => {
-
+  /**/
     removeEvents();
 
     // Sync from Cesium to Svelte
     for (let ev in scenario) {
+        console.log(ev);
         if (viewer[`${ev}Changed`]) {
             registeredEvents[`${ev}Changed`] = viewer[`${ev}Changed`].addEventListener((e: any) => {
                 if (!isUpdatingFromStore) {
@@ -29,7 +30,8 @@ const initEvents = (viewer: Viewer) => {
         }
     }
 
-    for (let ev in dataSourceEvents) {
+    /*for (let ev in dataSourceEvents) {
+        console.log(ev);
         if (viewer.dataSources[ev]) {
             registeredEvents[ev] = viewer.dataSources[ev].addEventListener(debounce((e: any) => {
                 if (!isUpdatingFromStore) {
@@ -37,29 +39,31 @@ const initEvents = (viewer: Viewer) => {
                 }
             }, 300));
         }
-    }
+    }*/
 
-    // Sync from Svelte to Cesium
-    for (let ev in scenario) {
-        if (scenario[ev]?.subscribe) {
-            registeredStoreSubscriptions[ev] = scenario[ev].subscribe(debounce(value => {
-                if (viewer[ev] !== value) {
-                    isUpdatingFromStore = true;
-                    viewer[ev] = value;
-                    isUpdatingFromStore = false;
-                }
-            }, 300));
-        }
+  // Sync from Svelte to Cesium
+  for (let ev in scenario) {
+    if (scenario[ev]?.subscribe) {
+      registeredStoreSubscriptions[ev] = scenario[ev].subscribe(
+        debounce((value) => {
+          if (viewer[ev] !== value) {
+            isUpdatingFromStore = true;
+            viewer[ev] = value;
+            isUpdatingFromStore = false;
+          }
+        }, 300)
+      );
     }
-}
+  }
+};
 
 const removeEvents = () => {
-    for (let x in registeredEvents) {
-        registeredEvents[x]();
-    }
-    for (let x in registeredStoreSubscriptions) {
-        registeredStoreSubscriptions[x]();
-    }
-}
+  for (let x in registeredEvents) {
+    registeredEvents[x]();
+  }
+  for (let x in registeredStoreSubscriptions) {
+    registeredStoreSubscriptions[x]();
+  }
+};
 
 export { initEvents, removeEvents, dataSourceEvents };
